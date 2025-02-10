4e377f842facf9e27f11febde04471918fb53241
livehybrid <5527349+livehybrid@users.noreply.github.com>
2025-02-10 11:10:38 +0000

    Update index.jsx

--- template/src/pages/index.jsx
+++ template/src/pages/index.jsx
@@ -8,13 +8,22 @@ import { format } from "util";
 import 'bootstrap/dist/css/bootstrap.css';
 import getScreenshotUrl from '../components/getScreenshotUrl';
 
-export default function Home() {
+export async function getStaticProps() {
+    const screenshotUrl = getScreenshotUrl("index");
+    return {
+        props: {
+            screenshotUrl,
+        },
+    };
+}
+
+export default function Home({ screenshotUrl }) {
     const { focusColor } = useSplunkTheme();
     const Footer = styled.p`
         color: ${focusColor};
-        text-align:center;
-    `
-    const screenshotUrl = getScreenshotUrl("index");
+        text-align: center;
+    `;
+
     return (
         <Page
             title={process.env.NEXT_PUBLIC_DASHPUBTITLE || 'Dashboards'}
@@ -22,12 +31,36 @@ export default function Home() {
             imageUrl={screenshotUrl}
             baseUrl={process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : null}>
             <Homepage key="home" />
-            { process.env.NEXT_PUBLIC_DASHPUBFOOTER!=="false" ?
-            <Footer align="center">
-                {process.env.NEXT_PUBLIC_DASHPUBFOOTER || "Hosted Splunk Dashboards" }
-                {process.env.NEXT_PUBLIC_DASHPUBHOSTEDBY ? <> by <Link to={process.env.NEXT_PUBLIC_DASHPUBHOSTEDURL || '#'} openInNewContext="">{process.env.NEXT_PUBLIC_DASHPUBHOSTEDBY}</Link> </> : " "}
-                using <Link to={process.env.NEXT_PUBLIC_DASHPUBREPO || "https://github.com/splunk/dashpub"} openInNewContext="">Dashpub</Link>
-            </Footer> : "" }
+            {process.env.NEXT_PUBLIC_DASHPUBFOOTER !== "false" ? (
+                <Footer>
+                    {process.env.NEXT_PUBLIC_DASHPUBFOOTER || "Hosted Splunk Dashboards"}
+                    {process.env.NEXT_PUBLIC_DASHPUBHOSTEDBY ? (
+                        <>
+                            {" by "}
+                            <Link
+                                to={process.env.NEXT_PUBLIC_DASHPUBHOSTEDURL || '#'}
+                                openInNewContext=""
+                            >
+                                {process.env.NEXT_PUBLIC_DASHPUBHOSTEDBY}
+                            </Link>{" "}
+                        </>
+                    ) : (
+                        " "
+                    )}
+                    using{" "}
+                    <Link
+                        to={
+                            process.env.NEXT_PUBLIC_DASHPUBREPO ||
+                            "https://github.com/splunk/dashpub"
+                        }
+                        openInNewContext=""
+                    >
+                        Dashpub
+                    </Link>
+                </Footer>
+            ) : (
+                ""
+            )}
         </Page>
     );
 }
