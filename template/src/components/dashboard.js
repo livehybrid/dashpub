a1553afcf3023ca0dd26684413dc080d9778458c
livehybrid <will@livehybrid.com>
2025-08-19 11:13:34 +0100

    WIP

--- template/src/components/dashboard.js
+++ template/src/components/dashboard.js
@@ -54,13 +54,14 @@ function updateAssetUrls(orig, { origin = window.location.origin } = {}) {
             images.add(viz.options.src);
         }
     }
-    if (def.layout.options.backgroundImage && def.layout.options.backgroundImage.src) {
+    if (def.layout && def.layout.options && def.layout.options.backgroundImage && def.layout.options.backgroundImage.src) {
         def.layout.options.backgroundImage.src = normalizeImageUrl(def.layout.options.backgroundImage.src);
         images.add(def.layout.options.backgroundImage.src);
     }
-    if (!def.layout.options.backgroundColor) {
-        def.layout.options.backgroundColor = '#ffffff';
-    }
+    // TODO: this is a hack to make the dashboard work without a background color
+    // if (!def.layout.options.backgroundColor) {
+    //     def.layout.options.backgroundColor = '#ffffff';
+    // }
     return [def, [...images].filter((img) => img != null)];
 }
 
